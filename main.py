e01a017d07438a1a30f7829ba2b72e08656f3d64
opielapatryk <xpatrykopiela@gmail.com>
2023-08-02 13:28:51 +0200

    start button, timer

--- main.py
+++ main.py
@@ -11,19 +11,10 @@ words = []
 for i in range(100):
     words.append(RandomWords().get_random_word())
 
-
 """set tkinter"""
 from tkinter import *
 
 win = Tk()  
-frame = Frame(win)
-frame.pack()
- 
-leftframe = Frame(win)
-leftframe.pack(side=LEFT)
- 
-rightframe = Frame(win)
-rightframe.pack(side=RIGHT)
 
 """centering window"""
 window_height = 900
@@ -37,10 +28,25 @@ y_cordinate = int((screen_height/2) - (window_height/2))
 
 win.geometry("{}x{}+{}+{}".format(window_width, window_height, x_cordinate, y_cordinate))
 
-"""display text"""
-Label(frame,text=words,wraplength=1200,font=("Arial",30)).pack()
-
 """set and start timer when keypress"""
+def timeout_message():
+    popup = Toplevel()
+    popup.title("Timeout")
+    popup.geometry("{}x{}+{}+{}".format(300, 100, x_cordinate+450, y_cordinate+400))
+    popup_label = Label(popup, text="Time's up!", font=("Arial", 20))
+    popup_label.pack()
+    popup.focus_set()
+    
+"""start_function"""
+def start():
+    """display text"""
+    start_button.pack_forget()
+    Label(text=words,wraplength=1200,font=("Arial",30)).pack()
+    win.after(30000,timeout_message)    
+
+"""bind start button"""
+start_button = Button(text="Start", font=("Arial",30),command=start)
+start_button.pack()
 
 
 win.mainloop()
